e56b8ba94fe9e3127bb073c9078eede35f2d475a
BugHunter-X <askzenai@gmail.com>
2025-02-23 16:52:06 +0100

    Add bugSolution.go file

---
+++ bugSolution.go
@@ -0,0 +1,26 @@
+```go
+package main
+
+import (
+
+        "fmt"
+        "sync"
+)
+
+func main() {
+        var wg sync.WaitGroup
+        var count int
+        var mu sync.Mutex // Add a mutex to protect the counter
+        for i := 0; i < 1000; i++ {
+                wg.Add(1)
+                go func() {
+                        defer wg.Done()
+                        mu.Lock() // Lock before accessing the counter
+                        count++
+                        mu.Unlock() // Unlock after accessing the counter
+                }()
+        }
+        wg.Wait()
+        fmt.Println(count)
+}
+```
